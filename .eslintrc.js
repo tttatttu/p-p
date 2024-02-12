2858bf51aac2c7883b32bb7d6311d7fae31e19e5
Tatiana Malashkina <t.malashkina@bi.zone>
2024-02-12 16:34:36 +0300

    Опции в линтере. Добавляем поддержку алисов в eslint плагин

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,7 +66,7 @@ module.exports = {
     "no-undef": "off",
     "react/no-array-index-key": "off",
     "arrow-body-style": "off",
-    "ulbi-tv-plugin/path-checker": "error",
+    "ulbi-tv-plugin/path-checker": ["error", { alias: "@" }],
   },
   globals: {
     __IS_DEV__: true,
