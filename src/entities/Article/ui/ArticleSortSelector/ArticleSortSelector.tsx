58692ddd9fb3309882ff213c5567959d04a4dc15
Tatiana Malashkina <t.malashkina@bi.zone>
2024-02-13 10:08:14 +0300

    Generic components

--- src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
+++ src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useMemo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { Select } from '@/shared/ui/Select';
+import { Select, SelectOption } from '@/shared/ui/Select';
 import { SortOrder } from '@/shared/types';
 import { ArticleSortField } from '../../model/consts/articleConsts';
 import cls from './ArticleSortSelector.module.scss';
@@ -19,31 +19,31 @@ export const ArticleSortSelector = memo(({
 }: ArticleSortSelectorProps) => {
     const { t } = useTranslation();
 
-    // const orderOptions = useMemo<SelectOption[]>(() => [
-    //     {
-    //         value: 'asc',
-    //         content: t('возрастанию'),
-    //     },
-    //     {
-    //         value: 'desc',
-    //         content: t('убыванию'),
-    //     },
-    // ], [t]);
+    const orderOptions = useMemo<SelectOption<SortOrder>[]>(() => [
+        {
+            value: 'asc',
+            content: t('возрастанию'),
+        },
+        {
+            value: 'desc',
+            content: t('убыванию'),
+        },
+    ], [t]);
 
-    // const sortFieldOptions = useMemo<SelectOption[]>(() => [
-    //     {
-    //         value: ArticleSortField.CREATED,
-    //         content: t('дате создания'),
-    //     },
-    //     {
-    //         value: ArticleSortField.TITLE,
-    //         content: t('названию'),
-    //     },
-    //     {
-    //         value: ArticleSortField.VIEWS,
-    //         content: t('просмотрам'),
-    //     },
-    // ], [t]);
+    const sortFieldOptions = useMemo<SelectOption<ArticleSortField>[]>(() => [
+        {
+            value: ArticleSortField.CREATED,
+            content: t('дате создания'),
+        },
+        {
+            value: ArticleSortField.TITLE,
+            content: t('названию'),
+        },
+        {
+            value: ArticleSortField.VIEWS,
+            content: t('просмотрам'),
+        },
+    ], [t]);
 
     const changeSortHandler = useCallback((newSort: string) => {
         onChangeSort(newSort as ArticleSortField);
@@ -56,13 +56,13 @@ export const ArticleSortSelector = memo(({
     return (
         <div className={classNames(cls.ArticleSortSelector, {}, [className])}>
             <Select
-                // options={sortFieldOptions}
+                options={sortFieldOptions}
                 label={t('Сортировать ПО')}
                 value={sort}
                 onChange={changeSortHandler}
             />
             <Select
-                // options={orderOptions}
+                options={orderOptions}
                 label={t('по')}
                 value={order}
                 onChange={changeOrderHandler}
