c94b78d47443a264225fac3f00260c6301595f81
Tatiana Malashkina <t.malashkina@bi.zone>
2024-02-14 11:55:21 +0300

    Тесты на роутер. Обновляем RTL

--- src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
+++ src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
@@ -2,7 +2,7 @@ import { createEntityAdapter, createSlice, PayloadAction } from '@reduxjs/toolki
 import { StateSchema } from '@/app/providers/StoreProvider';
 
 import { ARTICLES_VIEW_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
-import { SortOrder } from '@/shared/types';
+import { SortOrder } from '@/shared/types/sort';
 
 import { ArticlesPageSchema } from '../types/articlesPageSchema';
 import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
