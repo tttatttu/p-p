4620c061a89ae1236bb3619a1d65a20eea365024
Tatiana Malashkina <t.malashkina@bi.zone>
2024-02-29 17:57:31 +0300

    Редизайн Navbar, dropdowns, работа с текстом

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.stories.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.stories.tsx
@@ -1,17 +1,18 @@
+import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { NotificationButton } from './NotificationButton';
 
 export default {
-  title: 'features/NotificationButton',
-  component: NotificationButton,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    title: 'features/NotificationButton',
+    component: NotificationButton,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
 } as ComponentMeta<typeof NotificationButton>;
 
 const Template: ComponentStory<typeof NotificationButton> = (args) => (
-  <NotificationButton {...args} />
+    <NotificationButton {...args} />
 );
 
 export const Normal = Template.bind({});
