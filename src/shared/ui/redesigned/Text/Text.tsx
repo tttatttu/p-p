5e15ce427c75acd08d251fe5b83f335c91181298
Tatiana Malashkina <t.malashkina@bi.zone>
2024-03-04 12:44:42 +0300

    Редизайн ленты статей

--- src/shared/ui/redesigned/Text/Text.tsx
+++ src/shared/ui/redesigned/Text/Text.tsx
@@ -15,7 +15,7 @@ interface TextProps {
   variant?: TextVariant;
   align?: TextAlign;
   size?: TextSize;
-
+  bold?: boolean;
   'data-testid'?: string;
 }
 
@@ -41,6 +41,7 @@ export const Text = memo(
     variant = 'primary',
     align = 'left',
     size = 'm',
+    bold,
     'data-testid': dataTestId = 'Text',
   }: TextProps) => {
     const HeaderTag = mapSizeToHeaderTag[size];
@@ -49,7 +50,13 @@ export const Text = memo(
     const additionalClasses = [className, cls[variant], cls[align], sizeClass];
 
     return (
-      <div className={classNames(cls.Text, {}, additionalClasses)}>
+      <div
+        className={classNames(
+          cls.Text,
+          { [cls.bold]: bold },
+          additionalClasses,
+        )}
+      >
         {title && (
           <HeaderTag className={cls.title} data-testid={`${dataTestId}.Header`}>
             {title}
