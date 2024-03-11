e535adc4c9462d10675437c887bc3ed9e06ef08e
Tatiana Malashkina <t.malashkina@bi.zone>
2024-03-11 19:44:15 +0300

    Toolbar как часть Layout. ScrollToolbar. useCurrentRoute

---
+++ src/widgets/ScrollToolbar/ui/ScrollToolbar/ScrollToolbar.tsx
@@ -0,0 +1,22 @@
+import { memo } from 'react';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './ScrollToolbar.module.scss';
+import { VStack } from '@/shared/ui/redesigned/Stack';
+import { ScrollToTopButton } from '@/features/scrollToTopButton';
+
+interface ScrollToolbarProps {
+  className?: string;
+}
+
+export const ScrollToolbar = memo(({ className }: ScrollToolbarProps) => {
+  return (
+    <VStack
+      justify="center"
+      align="center"
+      max
+      className={classNames(cls.ScrollToolbar, {}, [className])}
+    >
+      <ScrollToTopButton />
+    </VStack>
+  );
+});
